8c92780b41aacc5c616ae2fd00a90c5fc62f47f4
Diego Chruscinski <contatodiegochruscinski@gmail.com>
2023-10-12 21:08:23 -0300

    Class "Department/Seller and test"

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -14,5 +14,6 @@ public class Program {
         Seller seller = new Seller(21,"Diego", "diego@gmail.com", new Date(), 3000.0, department);
 
         System.out.println(seller);
+        //
     }
 }
