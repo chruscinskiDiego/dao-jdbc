6cc1ea6e07c0d4a8cdbac7d531c6a469a21f3b22
Diego Chruscinski <contatodiegochruscinski@gmail.com>
2023-10-13 01:44:01 -0300

    FindById implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -10,13 +10,10 @@ import java.util.Date;
 public class Program {
 
     public static void main(String[] args) {
-        Department department = new Department(1,"Mine");
-        System.out.println(department);
 
-        Seller seller = new Seller(21,"Diego", "diego@gmail.com", new Date(), 3000.0, department);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
-        SellerDao seller1 = DaoFactory.createSellerDao();
         System.out.println(seller);
-        //
     }
 }
